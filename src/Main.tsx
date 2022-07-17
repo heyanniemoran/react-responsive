ea4511edcf6db6ce6caf47c4a3e019dc9390a71f
heyanniemoran <heyanniemoran@gmail.com>
2022-07-17 23:29:06 +0300

    Компонент для MediaQuery

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import useMediaQuery from "./Responsive";
+import useMediaQuery, { MediaQuery } from "./Responsive";
 
 export const Main = () => {
   const isDesktopOrLaptop: boolean = useMediaQuery({
@@ -15,12 +15,24 @@ export const Main = () => {
   const isRetina: boolean = useMediaQuery({ query: "(min-resolution: 2dppx)" });
   return (
     <div>
-      <h1>Device Test!</h1>
-      {isDesktopOrLaptop && <p>You are a desktop or laptop</p>}
-      {isBigScreen && <p>You have a huge screen</p>}
-      {isTabletOrMobile && <p>You are a tablet or mobile phone</p>}
-      <p>Your are in {isPortrait ? "portrait" : "landscape"} orientation</p>
-      {isRetina && <p>You are retina</p>}
+      <div>
+        <h1>Device Test!</h1>
+        {isDesktopOrLaptop && <p>You are a desktop or laptop</p>}
+        {isBigScreen && <p>You have a huge screen</p>}
+        {isTabletOrMobile && <p>You are a tablet or mobile phone</p>}
+        <p>Your are in {isPortrait ? "portrait" : "landscape"} orientation</p>
+        {isRetina && <p>You are retina</p>}
+      </div>
+      <div>
+        <h1>Device Test!</h1>
+        <MediaQuery minWidth={1224}>
+          <p>You are a desktop or laptop</p>
+          <MediaQuery minWidth={1824}>
+            <p>You also have a huge screen</p>
+          </MediaQuery>
+        </MediaQuery>
+        <MediaQuery minResolution="2dppx">123</MediaQuery>
+      </div>
     </div>
   );
 };
--- src/Responsive.tsx
+++ src/Responsive.tsx
@@ -1,15 +1,61 @@
 import React, { useState, useEffect } from "react";
 
-export default function useMediaQuery(query: any) {
-  let match_media = window.matchMedia(query.query).matches;
+export default function useMediaQuery(obj: { query: string }) {
+  let match_media = window.matchMedia(obj.query).matches;
   const [state, setState] = useState(match_media);
 
   useEffect(() => {
     window.addEventListener("resize", function () {
-      let match_media_effect = window.matchMedia(query.query).matches;
+      let match_media_effect = window.matchMedia(obj.query).matches;
       setState(match_media_effect);
     });
   });
 
   return state;
+}
+
+export function MediaQuery(props: any) {
+  const query_array = [];
+  const orientation: string = props.orientation;
+  if (orientation) query_array.push("(orientation: " + orientation + ")");
+  const minWidth: string = props.minWidth;
+  if (minWidth) query_array.push("(min-width: " + minWidth + "px)");
+  const maxWidth: string = props.maxWidth;
+  if (maxWidth) query_array.push("(max-width: " + maxWidth + "px)");
+  const minHeight: string = props.minHeight;
+  if (minHeight) query_array.push("(min-height: " + minHeight + "px)");
+  const maxHeight: string = props.maxHeight;
+  if (maxHeight) query_array.push("(max-height: " + maxHeight + "px)");
+  // if (window.matchMedia("(max-height: " + maxHeight + ")").matches)
+  //   return <div>{props.children}</div>;
+
+  const minResolution: string | number = props.minResolution;
+  if (minResolution)
+    query_array.push(
+      "(" +
+        (typeof minResolution === "string"
+          ? "min-resolution"
+          : "-webkit-min-device-pixel-ratio") +
+        ": " +
+        minResolution +
+        ")"
+    );
+  const maxResolution: string | number = props.maxResolution;
+  if (maxResolution)
+    query_array.push(
+      "(" +
+        (typeof maxResolution === "string"
+          ? "max-resolution"
+          : "-webkit-max-device-pixel-ratio") +
+        ": " +
+        maxResolution +
+        ")"
+    );
+
+  const query = query_array.join(" and ");
+  const res = useMediaQuery({ query: query });
+  // debugger;
+  if (!res) return null;
+
+  return <div>{props.children}</div>;
 }
